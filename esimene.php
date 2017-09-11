e17849b3121b3da3ea65d2df9ce0035000365e31
Kristo Roots <rootkris@users.noreply.github.com>
2017-09-11 10:37:06 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Kristo Roots veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Kristo Roots</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	<p>Olen Kristo Roots, tulen Pärnust.</p>
+	<p>Hobideks on jalgpall ja korvpall.</p>
+	
+	<?php
+		echo "<p>Täna on vastik ilm!</p>";
+	?>
+</body>
+</html>
